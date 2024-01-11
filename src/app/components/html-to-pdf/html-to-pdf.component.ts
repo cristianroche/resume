26b2b75cabe76b586980729f618ffa0a5b61598b
Cristian Roche <cristian@factoryfix.com>
2024-01-10 21:26:46 -0300

    fix pdf

--- src/app/components/html-to-pdf/html-to-pdf.component.ts
+++ src/app/components/html-to-pdf/html-to-pdf.component.ts
@@ -1,28 +1,30 @@
 import { Component } from '@angular/core';
-import jsPDF from "jspdf";
+import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 import * as personalData from '../../../assets/data/personal_data.json';
-
 
 @Component({
   selector: 'app-html-to-pdf',
   templateUrl: './html-to-pdf.component.html',
-  styleUrls: ['./html-to-pdf.component.scss']
+  styleUrls: ['./html-to-pdf.component.scss'],
 })
 export class HtmlToPdfComponent {
   personal_data = personalData;
 
-  public async  exportPDF() {
-
-    html2canvas(document.body).then(canvas => {
-      const pdfName = `${this.personal_data.name.split(" ").join("-").toLowerCase()}-cv.pdf`
+  public async exportPDF() {
+    html2canvas(document.body).then((canvas) => {
+      const pdfName = `${this.personal_data.name
+        .split(' ')
+        .join('-')
+        .toLowerCase()}-cv.pdf`;
 
       const doc = new jsPDF('p', 'mm');
+
       const contentDataURL = canvas.toDataURL('image/png');
 
       const pageWidth = doc.internal.pageSize.getWidth();
       const pageHeight = doc.internal.pageSize.getHeight();
-      const imgHeight = canvas.height * pageWidth / canvas.width;
+      const imgHeight = (canvas.height * pageWidth) / canvas.width;
       let heightLeft = imgHeight;
 
       let position = 0;
